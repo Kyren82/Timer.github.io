3497cbc8906cf92851a206a4d3f376f2ddf1271a
Kyren82 <erykkal123@gmail.com>
2026-06-30 13:47:41 +0200

    all three files are now updated & fixed

--- todo.php
+++ todo.php
@@ -1,3 +1,40 @@
+<?php
+$day = isset($_GET["day"]) ? $_GET["day"] : (isset($_POST["day"]) ? $_POST["day"] : date("d"));
+
+if (isset($_POST["text"])) {
+    $text = $_POST["text"];
+    $formatted_date = date("Y-m-") . sprintf("%02d", $day);
+
+    $conn = mysqli_connect("localhost", "root", "", "staz");
+
+    if ($conn) {
+        $text_escaped = mysqli_real_escape_string($conn, $text);
+        $date_escaped = mysqli_real_escape_string($conn, $formatted_date);
+
+        $check_query = "SELECT * FROM dni WHERE dzien = '$date_escaped'";
+        $result = mysqli_query($conn, $check_query);
+
+        if (!$result) {
+            die("Błąd zapytania SELECT: " . mysqli_error($conn));
+        }
+
+        if (mysqli_num_rows($result) > 0) {
+            $query = "UPDATE dni SET opis = '$text_escaped' WHERE dzien = '$date_escaped'";
+        } else {
+            $query = "INSERT INTO dni (dzien, opis) VALUES ('$date_escaped', '$text_escaped')";
+        }
+
+        if (!mysqli_query($conn, $query)) {
+            die("Błąd zapytania zapisu (INSERT/UPDATE): " . mysqli_error($conn) . "<br>Zapytanie: " . $query);
+        }
+
+        mysqli_close($conn);
+
+        header("Location: todo.php?day=" . urlencode($day));
+        exit();
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,43 +46,13 @@
 <body>
     <div id="wrapper">
         <div id="header">
-            <?php
-                $day = isset($_GET["day"]) ? $_GET["day"] : (isset($_POST["day"]) ? $_POST["day"] : date("d"));
-                echo("<p>dzień ".$day.". Co zrobiłeś?</p>");
-            ?>
+            <?php echo("<p>dzień ".htmlspecialchars($day).". Co zrobiłeś?</p>"); ?>
         </div>
         <div id="main">
-            <form action="todo.php" method="post">
+            <form action="todo.php?day=<?php echo urlencode($day); ?>" method="post">
                 <input type="hidden" name="day" value="<?php echo htmlspecialchars($day); ?>">
                 <input type="text" name="text" id="text" required>
                 <button type="submit">Prześlij</button>
-                <?php
-                    if(isset($_POST["text"])){
-                        $text = $_POST["text"];
-                        $formatted_date = date("Y-m-d"); 
-                        
-                        $conn = mysqli_connect("localhost", "root", "", "staz");
-                        
-                        if ($conn) {
-                            $text_escaped = mysqli_real_escape_string($conn, $text);
-                            
-                            $check_query = "SELECT * FROM dni WHERE dzien = '$formatted_date'";
-                            $result = mysqli_query($conn, $check_query);
-                            
-                            if (mysqli_num_rows($result) > 0) {
-                                $query = "UPDATE dni SET opis = '$text_escaped' WHERE dzien = '$formatted_date'";
-                            } else {
-                                $query = "INSERT INTO dni (dzien, opis) VALUES ('$formatted_date', '$text_escaped')";
-                            }
-                            
-                            mysqli_query($conn, $query);
-                            mysqli_close($conn);
-                            
-                            header("Location: todo.php?day=".$day);
-                            exit();
-                        }
-                    }
-                ?>
             </form>
         </div>
         <div id="footer">
